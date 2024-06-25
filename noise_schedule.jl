194ff952e9caf2b8e5a842cab3a96c9374daf0d5
Jack Elliot Collier Ryder <jack.collier.ryder@gmail.com>
2024-06-25 16:55:28 +0200

    implemented the noise schedule parameterization as defined by the "Simple and Effective Masked Diffusion Language Models" paper

--- noise_schedule.jl
+++ noise_schedule.jl
@@ -1,1 +1,41 @@
+# Define the noise schedule functions 
 
+function loglinear(t::Real)
+    return -log10(1-t)
+end
+
+function cosinesquared(t::Real)
+    return -log10((cos((pi/2)*(1-t)))^2)
+end
+
+function cosineschedule(t::Real)
+    return -log10(cos((pi/2)*(1-t)))
+end
+
+function linear(t::Real, σ_max = 10^8)
+    return σ_max * t
+end
+
+
+function α_t(t::Real, σ::Function = linear)
+
+    """Calculates the noise schedule parameter α_t.
+
+
+        # Arguments
+        - `t::Real`: The time step, a real number in the range [0, 1].
+        - `σ::Function`: A function representing the noise schedule. 
+        Defaults to `linear`.
+
+        # Returns
+        - A real number representing the noise schedule parameter α_t at time `t`.
+
+        # Noise schedule options
+        - `linear(t)`: Linear noise schedule.
+        - `cosineschedule(t)`: Cosine noise schedule.
+        - `cosinesquared(t)`: Cosine squared noise schedule.
+        - `loglinear(t)`: Log-linear noise schedule.
+    """
+
+    return exp(-σ(t))
+end
