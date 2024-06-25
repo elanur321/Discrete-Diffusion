8ec180c0c6e5f16a266a47e6530b8b220b10c8c7
Jack Elliot Collier Ryder <jack.collier.ryder@gmail.com>
2024-06-25 14:33:17 +0200

    created barebone structure for first functions

--- diffusion.jl
+++ diffusion.jl
@@ -1,1 +1,19 @@
 
+
+function forward_mask(x::Vector, t::Float64)
+    """
+    Function for forward masking process described in chapter 3.2.1
+    """
+end
+
+function reverse_unmask()
+    """
+    Function for reverse unmasking process described in chapter 3.2.2
+    """
+end
+
+function loss()
+    """
+    Function for computing loss function - primarily based on section 3.5
+    """
+end
