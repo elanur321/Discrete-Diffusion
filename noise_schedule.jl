98ab733faa7ef1ba0c6b77b1d5471cf85e5b4cf2
Jack Elliot Collier Ryder <jack.collier.ryder@gmail.com>
2024-06-26 13:51:42 +0200

    fixed comments

--- noise_schedule.jl
+++ noise_schedule.jl
@@ -1,13 +1,11 @@
 # Define the noise schedule functions 
 
-function α_t(t::Real, σ::Function = linear)
+function α_t(t::Real)
 
     """Calculates the noise schedule parameter α_t based on the cosine noise scheduler
 
         # Arguments
         - `t::Real`: The time step, a real number in the range [0, 1].
-        - `σ::Function`: A function representing the noise schedule. 
-        Defaults to `linear`.
 
         # Returns
         - A real number representing the noise schedule parameter α_t at time `t`.
