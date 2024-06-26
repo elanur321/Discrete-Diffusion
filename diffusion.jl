75fe042668af583999ab8a7672e78a46dabb1b03
Jack Elliot Collier Ryder <jack.collier.ryder@gmail.com>
2024-06-26 11:56:20 +0200

    started implementation of forward diffusion process

--- diffusion.jl
+++ diffusion.jl
@@ -1,13 +1,40 @@
+include("noise_schedule.jl")
 
-struct MaskedDiffusionLanguageModel
+struct MaskedDiffusionLanguageModel 
+
+    vocab_size::Int
+    mask_token_id::Int
+    noise_schedule::Function
 
 end
 
-function forward(process::MaskedDiffusionLanguageModel, x_s::AbstractArray, s::Real, t::Real)\
+function forward(process::MaskedDiffusionLanguageModel, x_s::AbstractArray, t::Real)\
     """
     Function for forward masking process described in chapter 3.2.1
     """
+    z_t = similar(x_s)
+    for (i, value) in enumerate(x_s)
+
+        p_keep = α_t(t, process.noise_schedule)
+
+        if rand() < p_keep
+            z_t[i] = value
+        else z_t[i] = process.mask_token_id end
+    end
+    return z_t
 end
+
+
+############# Example #################
+
+vocab = [1, 2, 3]
+ex = MaskedDiffusionLanguageModel(length(vocab), length(vocab) + 1, loglinear)
+
+@show vocab = forward(ex, vocab, 0.1)
+@show vocab = forward(ex, vocab, 0.2)
+@show vocab = forward(ex, vocab, 0.3)
+
+######################################
 
 function backward(process::MaskedDiffusionLanguageModel, x_t::AbstractArray, s::Real, t::Real)
     """
