c6ed5b837552f479062bcf7376eb07e686818837
Jack Elliot Collier Ryder <jack.collier.ryder@gmail.com>
2024-06-26 13:50:58 +0200

    changed noise scheduling to fix bug - now only the cosine scheduler is implemented

--- noise_schedule.jl
+++ noise_schedule.jl
@@ -1,26 +1,8 @@
 # Define the noise schedule functions 
 
-function loglinear(t::Real)
-    return -log10(1-t)
-end
-
-function cosinesquared(t::Real)
-    return -log10((cos((pi/2)*(1-t)))^2)
-end
-
-function cosineschedule(t::Real)
-    return -log10(cos((pi/2)*(1-t)))
-end
-
-function linear(t::Real, σ_max = 10^8)
-    return σ_max * t
-end
-
-
 function α_t(t::Real, σ::Function = linear)
 
-    """Calculates the noise schedule parameter α_t.
-
+    """Calculates the noise schedule parameter α_t based on the cosine noise scheduler
 
         # Arguments
         - `t::Real`: The time step, a real number in the range [0, 1].
@@ -29,13 +11,7 @@ function α_t(t::Real, σ::Function = linear)
 
         # Returns
         - A real number representing the noise schedule parameter α_t at time `t`.
-
-        # Noise schedule options
-        - `linear(t)`: Linear noise schedule.
-        - `cosineschedule(t)`: Cosine noise schedule.
-        - `cosinesquared(t)`: Cosine squared noise schedule.
-        - `loglinear(t)`: Log-linear noise schedule.
     """
 
-    return exp(-σ(t))
+    return cos(pi * t/2)
 end
