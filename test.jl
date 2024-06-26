c8d964bda8c2df9687e465b3ba92913de9862698
Jack Elliot Collier Ryder <jack.collier.ryder@gmail.com>
2024-06-26 16:12:38 +0200

    created some easy tests which control that forward masking works and check that the SUBS parameterization is correctly applied during reverse unmasking

---
+++ test.jl
@@ -0,0 +1,41 @@
+using Test
+include("diffusion.jl")
+
+MASK = [0, 0, 0, 1]
+model = MaskedDiffusionLanguageModel(3, MASK)
+
+####### Test 1: Ensure that forward model converts vectorized data correctly ###########
+
+@test forward(model, [[1,0,0,0], [0,1,0,0], [0,0,1,0]], 1) == [MASK, MASK, MASK]
+
+####### Test 2: Ensure that backward model follows the SUBS parameterization ###########
+
+function update_x!(model, x, i)
+    global x
+    x = backward(model, x, i, i)
+end
+
+function mask_indices(ls::AbstractArray)
+    indices = Set()
+    for (i, value) in ls 
+        if value == MASK
+            push!(indices, i)
+        end
+    end
+    return indices
+end
+
+global x = [MASK, MASK, MASK]
+
+for i in range(0, 1, step=0.1)
+    x_before = deepcopy(x)
+    update_x!(model, x, i)
+    @test length(setdiff(mask_indices(x_before), mask_indices(x))) == 0
+    
+    for j in x
+        if j not in mask_indices(x_before)
+            @test x_before[j] == x[j]
+        end
+    end
+end
+
