f9236ddda736adf530ec1bea0a579512e78a3b0a
Jack Elliot Collier Ryder <jack.collier.ryder@gmail.com>
2024-06-26 14:19:52 +0200

    started implementation of loss function

--- loss.jl
+++ loss.jl
@@ -1,11 +1,23 @@
 # Implementation of the Masked Diffusion Language Model loss function
+using Zygote
+using LinearAlgebra
+include("noise_schedule.jl")
+include("diffusion.jl")
+
+# TODO: optimize for GPU & take average over several time steps - i.e. implement integration part of equation
 
 function standardloss(
     p::MaskedDiffusionLanguageModel,
     t::Union{Real, AbstractVector{<: Real}},
     x̂, x;
-    scaler = defaultscaler
 )
-
-    return 
+    return (gradient(x -> α_t(x), t))[1]/(1 - α_t(t)) * sum([log(x̂[n] ⋅ value) for (i, value) in enumerate(x)])
 end
+
+############# Example #################
+
+ex = MaskedDiffusionLanguageModel(3, 4)
+
+standardloss(ex, 0.2, [2, 3, 1], [1, 2, 3])
+
+######################################
