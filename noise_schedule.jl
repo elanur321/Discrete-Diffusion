e14e9b9c121f100580ffb1d1ab52629000853495
Jack Elliot Collier Ryder <jack.collier.ryder@gmail.com>
2024-06-27 11:07:10 +0200

    added linear noise scheduler

--- noise_schedule.jl
+++ noise_schedule.jl
@@ -28,3 +28,17 @@ function loglinear(t::Real)
 
     return exp(log(1-t))
 end
+
+function linear(t::Real)
+
+    """Calculates the noise schedule parameter α_t based on the loglinear noise scheduler
+
+        # Arguments
+        - `t::Real`: The time step, a real number in the range [0, 1].
+
+        # Returns
+        - A real number representing the noise schedule parameter α_t at time `t`.
+    """
+
+    return 1 - t
+end
