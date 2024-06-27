94b458b72efda0596220f530435d9b375504b140
Jack Elliot Collier Ryder <jack.collier.ryder@gmail.com>
2024-06-27 11:57:05 +0200

    reduced redundant comments

--- noise_schedule.jl
+++ noise_schedule.jl
@@ -1,44 +1,13 @@
 # Define the noise schedule functions 
 
-
 function cosineschedule(t::Real)
-
-    """Calculates the noise schedule parameter α_t based on the cosine noise scheduler
-
-        # Arguments
-        - `t::Real`: The time step, a real number in the range [0, 1].
-
-        # Returns
-        - A real number representing the noise schedule parameter α_t at time `t`.
-    """
-
     return cos(pi * t/2)
 end
 
 function loglinear(t::Real)
-
-    """Calculates the noise schedule parameter α_t based on the loglinear noise scheduler
-
-        # Arguments
-        - `t::Real`: The time step, a real number in the range [0, 1].
-
-        # Returns
-        - A real number representing the noise schedule parameter α_t at time `t`.
-    """
-
     return exp(log(1-t))
 end
 
 function linear(t::Real)
-
-    """Calculates the noise schedule parameter α_t based on the loglinear noise scheduler
-
-        # Arguments
-        - `t::Real`: The time step, a real number in the range [0, 1].
-
-        # Returns
-        - A real number representing the noise schedule parameter α_t at time `t`.
-    """
-
     return 1 - t
 end
