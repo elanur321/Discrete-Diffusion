1389310198cbfbd6e35473c8a5919d94be875d25
Jack Elliot Collier Ryder <jack.collier.ryder@gmail.com>
2024-06-27 10:42:10 +0200

    implemented functions necessary for compatibility with diffusions.jl package

--- diffusion.jl
+++ diffusion.jl
@@ -10,7 +10,7 @@ end
 
 # TODO: change to one-hot vector representations of tokens instead of single numbers
 
-function forward(process::MaskedDiffusionLanguageModel, x_s::AbstractArray, t::Real)\
+function forward(process::MaskedDiffusionLanguageModel, x_s::AbstractArray, s::Real, t::Real)\
     """
     Function for forward masking process described in chapter 3.2.1
     """
@@ -32,9 +32,9 @@ end
 vocab = [1, 2, 3]
 ex = MaskedDiffusionLanguageModel(length(vocab), length(vocab) + 1, cosineschedule)
 
-@show vocab = forward(ex, vocab, 0.1)
-@show vocab = forward(ex, vocab, 0.2)
-@show vocab = forward(ex, vocab, 0.3)
+@show vocab = forward(ex, vocab, 0, 0.1)
+@show vocab = forward(ex, vocab, 0, 0.2)
+@show vocab = forward(ex, vocab, 0, 0.3)
 
 ######################################
 
@@ -45,7 +45,7 @@ function backward(process::MaskedDiffusionLanguageModel, x_theta::AbstractArray,
     z_t = copy(x_t)
     
     for i in eachindex(z_t)         #TODO: figure out how eachindex() works
-        if z_t[i] != process.masked_token_ID     
+        if z_t[i] != process.masked_token_ID # SUBS parameterization
             z_s[i] = z_t[i]
 
         else z_t[i] == process.masked_token_ID   
@@ -61,4 +61,13 @@ function backward(process::MaskedDiffusionLanguageModel, x_theta::AbstractArray,
     end
 
     return z_s
+end
+
+_sampleforward(rng::AbstractRNG, process::MaskedDiffusionLanguageModel, t::Real, x::AbstractArray) =
+    sample(rng, forward(process, x, 0, t))
+
+function _endpoint_conditioned_sample(rng::AbstractRNG, process::MaskedDiffusionLanguageModel, s::Real, t::Real, x_0::AbstractArray, x_t::AbstractArray)
+    prior = forward(process, x_0, 0, s)
+    likelihood = backward(process, x_t, s, t)
+    return sample(rng, combine(prior, likelihood))
 end
