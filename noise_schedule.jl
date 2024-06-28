8d377d1e34fe273c6cf388da742a21c8771b0fd9
Jack Elliot Collier Ryder <jack.collier.ryder@gmail.com>
2024-06-28 10:35:14 +0200

    calculated noise schedule gradients by hand to improve speed of loss function

--- noise_schedule.jl
+++ noise_schedule.jl
@@ -1,13 +1,13 @@
 # Define the noise schedule functions 
 
 function cosineschedule(t::Union{Real,AbstractVector{<:Real}})
-    return cos.(pi .* t ./ 2)
+    return cos.(pi .* t ./ 2), (pi/2).*(-sin.(pi .* t ./ 2))
 end
 
 function loglinear(t::Union{Real,AbstractVector{<:Real}})
-    return exp.(log10.(1 .- t))
+    return exp.(log10.(1 .- t)), exp.(1 .- t).*(1 ./ (log(10)t .- log(10)))
 end
 
 function linear(t::Union{Real,AbstractVector{<:Real}})
-    return 1 .- t
+    return 1 .- t, - t ./ t
 end
