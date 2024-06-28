c8fb6205b818dd12aa75ce2187b9bb33e967010e
Jack Elliot Collier Ryder <jack.collier.ryder@gmail.com>
2024-06-28 10:52:29 +0200

    improved loss function by using logitcrossentropy function

--- loss.jl
+++ loss.jl
@@ -1,29 +1,27 @@
 # Implementation of the Masked Diffusion Language Model loss function
 using Zygote
+using Random
+using Flux.Losses
 using CUDA
 using LinearAlgebra
+using Statistics
+
 include("noise_schedule.jl")
 include("diffusion.jl")
 
+
 # TODO: Actually run code on Nvidia GPU to see if it works
 # TODO: Not sure if x̂ and x are CuMatrices och just normal Matrices
+# TODO: examining only the masked token indices rather than comparing the full true and approximate posterior distributions.
 
-function defaultscaler(p::MaskedDiffusionLanguageModel, t::Union{Real,AbstractVector{<:Real}})
-    α_t = p.α_t(t)
-    α_prime = gradient(p.α_t, t)[1]
-    return α_prime / (1 - α_t)
-end
+defaultscaler(x) = 1
 
 function standardloss(
     p::MaskedDiffusionLanguageModel,
     t::Union{Real,AbstractVector{<:Real}},
-    x̂::CuMatrix, 
-    x::CuMatrix;
+    x̂, x;
     scaler=defaultscaler
 )
-
-    α_t = p.α(t)
-    α_prime = gradient(p.α, t)[1]
-    
-    return( α_prime / (1 - α_t)) * log.(sum(x̂ .* x, dims=1))
+    α_t, α_prime = p.α(t)
+    return (α_prime/(1 .- α_t)) .* logitcrossentropy(x̂, x; dims = 1, agg = mean)
 end
