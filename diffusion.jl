eb6ad7948ef4cb686289fe2fbb97bd87d98e3204
Jack Elliot Collier Ryder <jack.collier.ryder@gmail.com>
2024-07-02 15:28:23 +0200

    removed some old code and improved structure

--- diffusion.jl
+++ diffusion.jl
@@ -21,8 +21,7 @@ struct MaskedDiffusionLanguageModel <: DiscreteStateProcess
 
 end
 
-# TODO: change to one-hot vector representations of tokens instead of single numbers
-"""
+
 function _sampleforward(rng::AbstractRNG, process::MaskedDiffusionLanguageModel, t::Real, x::AbstractArray)
 
     z_t = copy(x)
@@ -36,29 +35,7 @@ function _sampleforward(rng::AbstractRNG, process::MaskedDiffusionLanguageModel,
     end
     return z_t
 
-end"""
-
-" Possible implementation more efficient for GPU computations
-using CUDA
-
-function _sampleforward(rng::AbstractRNG, process::MaskedDiffusionLanguageModel, t::Real, x::AbstractArray)
-    # Move data to GPU
-    z_t = CUDA.fill(process.mask_token_id, size(x))
-    x_d = CuArray(x)
-    p_keep = process.α(t)[1]
-
-    # Generate random numbers on the GPU
-    rand_vals = CUDA.rand(size(x_d))
-
-    # Apply mask condition
-    mask = rand_vals .< p_keep
-    z_t[mask] .= x_d[mask]
-
-    return Array(z_t)  # Move data back to CPU if needed
 end
-"
-
-
 function _sampleforward(rng::AbstractRNG, process::MaskedDiffusionLanguageModel, t::Real, x::AbstractArray)
     # Create a copy of x
     z_t = fill(process.mask_token_id, size(x))
@@ -74,45 +51,8 @@ function _sampleforward(rng::AbstractRNG, process::MaskedDiffusionLanguageModel,
     return z_t
 end
 
-#_sampleforward(rng::AbstractRNG, process::MaskedDiffusionLanguageModel, t::Real, x::AbstractArray) =
-    #sample(rng, forward(process, x, 0, t))
 
-    function _endpoint_conditioned_sample(rng::AbstractRNG, process::MaskedDiffusionLanguageModel, s::Real, t::Real, x_0::AbstractArray, x_t::AbstractArray)
-    @assert 0 ≤ s < t ≤ 1 "Invalid time steps: require 0 ≤ s < t ≤ 1" #not sure if this is needed but il keep it here
-
-    # Move data to GPU
-    x_0 = CuArray(x_0), x_t = CuArray(x_t)
-
-    vocab_size = size(process.embedding, 1)
-    x_s = copy(x_t)
-    
-    alpha_s = process.α(s)[1]
-    alpha_t = process.α(t)[1]  
-
-    # Create a mask for non-masked tokens
-    non_masked = x_t .!= process.mask_token_id
-
-    # Compute unnormalized log probabilities for all non-masked tokens
-    logits = (1 - alpha_s) .* log.(process.mask_vector[1:vocab_size-1]) .+ (alpha_s - alpha_t) .* x_0[1:vocab_size-1, :]
-
-    # Normalize / Compute probabilities using softmax
-    probs = vcat(softmax(logits, dims=1), zeros(1, size(logits, 2)))   
-
-    # make probabilities for mask 0
-    probs[process.mask_token_id, :] .= 0
-
-    # Sample tokens from categorical distribution. takes random number 0-1 and choses the word with the probability that matches
-    sampled_tokens = [rand(Categorical(probs[:, i])) for i in eachindex(probs, 2)]
-
-    # Combine non-masked tokens and sampled tokens
-    x_s = ifelse.(non_masked, x_t, sampled_tokens) #if nonmaksed x_s = x_t else = sampled token
-   
-    return x_s
-
-    "return sample(rng, combine(prior, x_s))"
-end
-
-# Quick-fix implementation
+# This implementation works for data that is NOT one-hot-encoded.
 function _endpoint_conditioned_sample(
     rng::AbstractRNG, 
     process::MaskedDiffusionLanguageModel, 
@@ -161,6 +101,7 @@ end
 # However, the above functions don't. 
 # Bellow are modified functions which should however serve this purpose.
 
+# TODO: potentially vectorize operations and optimize for GPU computations 
 
 function _sampleforward(rng::AbstractRNG, process::MaskedDiffusionLanguageModel, t::Real, x::AbstractVector{Bool})
     # Get the keep probability
@@ -232,7 +173,7 @@ function _endpoint_conditioned_sample(
         token_slice = (i-1)*vocab_size + 1 : i*vocab_size
         if xt[token_slice[process.mask_token_id]]
             probs = zeros(Float32, vocab_size)
-            probs[process.mask_token_id] = 1 - α_s
+            probs[process.mask_token_id] = 1 - α_s # CHECK
             predicted_token = findfirst(x0[token_slice])
             probs[predicted_token] = α_s - α_t
             probs ./= sum(probs)
